8a2bce77a46d92064ccc1c837d6c69b6e968cd58
Jake <jplane91@gmail.com>
2019-08-20 08:59:55 -0400

    first commit

---
+++ CoffeeShop/Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using CoffeeShop.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace CoffeeShop.Controllers
+{
+    public class RegistrationController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new PersonModel() { });
+        }
+
+        [HttpPost]
+        public IActionResult EnrollUser(PersonModel person)
+        {
+     
+
+            if (ModelState.IsValid)
+            {
+                return View(person);
+            }
+            else
+            {
+                return View("Index", person);
+            }
+        }
+    }
+}
---
+++ CoffeeShop/Models/PersonModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class PersonModel
+    {
+
+        [Required(ErrorMessage = "Must enter in a name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Must enter in a last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Must enter in a username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Must enter in an email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Must confirm email")]
+        [Compare("Email", ErrorMessage = "The emails do not match.")]
+        public string ConfirmEmail { get; set; }
+
+        [Required(ErrorMessage = "Must enter in a password")]
+        [RegularExpression(@"^[a-zA-Z]\w{3,14}$", ErrorMessage = "Password must between 4-15 characters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Must confirm password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+        public int Age { get; set; }
+
+    }
+}
